6043c81d05063712e66d475b2d5731f880a1c23d
Abhay <kabhay849@gmail.com>
2023-04-10 03:35:16 +0530

    first  commit

---
+++ Boilerplate.java
@@ -0,0 +1,10 @@
+public class Boilerplate {
+  public static void main(String args[]) {
+    System.out.print("HelloWorldHelloWorld");
+    System.out.println("HelloWorldHelloWorld");  //Next output will be in next line
+    System.out.print("HelloWorld\nHelloWorld");  //Where it encounter \n complier moves to next line(Just like in python or JS)
+
+
+  }
+}
+//Boiler plate code
