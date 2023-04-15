f1bd7f94d06eb756a0f4c0750f5ea81eb9497474
Abhay <kabhay849@gmail.com>
2023-04-16 01:49:14 +0530

    learning updated

---
+++ Typecasting.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Typecasting {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a= (int)45.7575f;  //conversion explicitily done by user even after lossy conversion take place.
+        System.out.println(a);
+
+        //char -->int,float,long,double(due to ascii value(watch ascii table for more info))
+        char b='c';
+        int c=b;
+        System.out.println(c);
+
+    }
+}
---
+++ Typeconversion.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Typeconversion {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int a=19;
+            float b=a;  //java compiler implicitily converted int into float.
+            System.out.println(b);
+        }
+    
+}
+//What is Type conversion all written on notes.
