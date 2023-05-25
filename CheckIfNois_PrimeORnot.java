b302707faaa1d6adbbdb1150c366f72e58db555b
Abhay <kabhay849@gmail.com>
2023-05-26 03:12:48 +0530

    updated

---
+++ CheckIfNois_PrimeORnot.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CheckIfNois_PrimeORnot {
+    // only for n>=2
+    public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) { // optimised
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(isPrime(n));
+
+    }
+
+}
