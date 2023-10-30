ed4f03f8f8add6a5b9014b2f7fd3967a411f3b48
Abhay <kabhay849@gmail.com>
2023-10-31 04:42:46 +0530

    commiting to save changes

---
+++ Recursion/BinaryStringProblem.java
@@ -0,0 +1,8 @@
+package Recursion;
+
+public class BinaryStringProblem {
+
+    public static void main(String[] args) {
+
+    }
+}
--- Testing.java
+++ Testing.java
@@ -1,29 +1,52 @@
+
 public class Testing {
-    public static void printArray(int arr[]) {
+    public static void printarray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
-    public static void leftshiftby1place(int nums[], int k) {
-        for (int i = k; i < nums.length; i++) {
-            nums[i - 1] = nums[i];
+    public static int[] addx(int arr[], int n, int x) {
+        int newarr[] = new int[n + 1];
+        for (int i = 0; i < arr.length; i++) {
+            newarr[i] = arr[i];
         }
-        nums[nums.length - 1] = 0;
+        newarr[n] = x;
+        return newarr;
     }
 
-    public static void moveZeroes(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == 0) {
-                leftshiftby1place(nums, i + 1);
-            }
+    public static int[] sortedArray(int[] a, int[] b) {
+        int i = 0;
+        int j = 0;
+        int op[] = new int[0];
+        if (a[i] <= b[j]) {
+            op = addx(op, op.length, a[i]); // appending using helper
+            i++;
+        } else {
+            op = addx(op, op.length, b[j]); // appending using helper
+            j++;
         }
+        while (i < a.length || j < b.length) {
+            if (a[i] <= b[j]) {
+                if (a[i] > op[op.length - 1]) {
+                    op = addx(op, op.length, a[i]);
+                    i++;
+                }
+            } else {
+                if (b[j] > op[op.length - 1]) {
+                    op = addx(op, op.length, b[j]);
+                    j++;
+                }
+            }
+
+        }
+        return op;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 0, 0, 1 };
-        moveZeroes(arr);
-        printArray(arr);
+        int a[] = { 1, 1, 2, 3, 4, 5 };
+        int b[] = { 2, 3, 4, 4, 5 };
+        printarray(sortedArray(a, b));
     }
 
 }
