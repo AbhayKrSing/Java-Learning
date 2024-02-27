f80eb0dfd896cfd8df2f93c4a1c14989fadc452f
Abhay <kabhay849@gmail.com>
2024-02-27 17:22:33 +0530

    updated

--- backTracking/NQueen.java
+++ backTracking/NQueen.java
@@ -1,8 +1,27 @@
 package backTracking;
 
 public class NQueen {
+    public static void print(char board[][]) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void Nqueens(char[][] board, int row) {
         // base case
+        if (board.length == row) {
+            print(board);
+            return;
+        }
+        // kaam and fn call
+        for (int j = 0; j < board[0].length; j++) {
+            board[row][j] = 'Q';
+            Nqueens(board, row + 1);
+            board[row][j] = '.';
+        }
 
     }
 
@@ -15,5 +34,6 @@ public class NQueen {
                 board[i][j] = '.';
             }
         }
+        Nqueens(board, 0);
     }
 }
